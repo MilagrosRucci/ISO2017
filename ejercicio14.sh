bbea479e2f64b11d75e3aa4f3a5ed4dc3b90f2db
Echeva <yaniiecheva@gmail.com>
2017-09-29 12:01:12 -0300

    Update ejercicio14.sh

--- ejercicio14.sh
+++ ejercicio14.sh
@@ -20,6 +20,9 @@ for arc in $archivos; do
 	echo $arc
 	if [ $2 == -a ]; then
 		cp $1/$arc $1/$arc$3
+		#Renombrar se hace en realidad con el mv de esta misma forma. o sea, mv $1/$arc $1/$arc$3
+		#el copy 'cp' mantiene dos archivos, con difentes nombres el move 'mv' elimina el primero y lo guarda con el
+		#segundo nombre
 	fi
 	if [ $2 == -b ]; then
 		cp $1/$arc $1/$3$arc
